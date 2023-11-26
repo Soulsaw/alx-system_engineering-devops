3ec5390f63293d91c6966710ba22dc0de20fa3cf
Soulsaw <ssawadogo292@gmail.com>
2023-11-26 00:10:56 +0000

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # This puppet script permit to install flask
-package{'flask':
-ensure   => installed,
-name     => 'flask==2.1.0',
+package{'Flask':
+ensure   => '2.1.0',
+name     => 'flask',
 provider => 'pip3',
 }
