6c853d2f64018f92882df0e3e420838df2242fb6
Soulsaw <ssawadogo292@gmail.com>
2023-11-30 02:56:04 +0000

    last update

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,39 @@
+# This puppet
+package {'nginx':
+ensure   => installed,
+name     => 'nginx',
+provider => 'apt',
+}
+
+file {'/etc/nginx/sites-available/default':
+    ensure  => file,
+    mode    => '0644',
+    content => "server {
+            listen 80 default server;
+            listen [::]:80;
+
+            location / {
+                return 200 "Hello word!";
+            }
+            location /redirect_me {
+                return 301 /redirect_me;
+            }
+    }",
+    notify  => Service['nginx'],
+}
+
+file {'/etc/nginx/sites-available/default':
+    ensure => link,
+    target => '/etc/nginx/sites-available/default',
+    notify => Service['nginx'],
+}
+
+file {'/etc/nginx/sites-available/default':
+    ensure => absent,
+}
+
+service {'nginx':
+ensure  => running,
+enable  => true,
+require => Package['nginx']
+}
