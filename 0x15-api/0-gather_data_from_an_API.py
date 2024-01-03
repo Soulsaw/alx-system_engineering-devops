1f6f0f425e5752883c02a034c91cd2bffbf50748
Soulsaw <ssawadogo292@gmail.com>
2024-01-03 09:50:30 +0000

    fix all

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,15 +4,18 @@ if __name__ == "__main__":
     import requests
     from sys import argv
     idUser = argv[1]
-    url = f"https://jsonplaceholder.typicode.com/users/{idUser}/todos"
-    url1 = f"https://jsonplaceholder.typicode.com/users/{idUser}"
-    with requests.get(url1) as response:
-        json = response.json()
-        EMPLOYEE_NAME = json['name']
-    with requests.get(url) as response:
-        json = response.json()
-        TOTAL_NUMBER_OF_TASKS = len(json)
-        NUMBER_OF_DONE_TASKS = len([x for x in json if x['completed']])
-        print("Employee {} is done with tasks({}/{}):".format(
-            EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-        [print('\t', x['title']) for x in json if x['completed']]
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    id = int(argv[1])
+    users = requests.get(users_url).json()
+    for user in users:
+        if user['id'] == id:
+            name = user['name']
+            break
+    todos = requests.get(todos_url).json()
+    task_total = [todo for todo in todos if todo['userId'] == id]
+    task_completed = [todo for todo in todos if todo['completed']
+                      and todo['userId'] == id]
+    print("Employee {} is done with tasks({}/{}):".format(
+        name, len(task_completed), len(task_total)))
+    [print('\t', x['title']) for x in task_completed]
