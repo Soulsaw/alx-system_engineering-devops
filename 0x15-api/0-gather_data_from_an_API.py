6aca6be5a72f6328d642264f906ac79114bf70da
Soulsaw <ssawadogo292@gmail.com>
2024-01-03 10:14:35 +0000

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,19 +3,12 @@
 if __name__ == "__main__":
     import requests
     from sys import argv
-    idUser = argv[1]
-    users_url = "https://jsonplaceholder.typicode.com/users"
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
-    id = int(argv[1])
-    users = requests.get(users_url).json()
-    for user in users:
-        if user['id'] == id:
-            name = user['name']
-            break
-    todos = requests.get(todos_url).json()
-    task_total = [todo for todo in todos if todo['userId'] == id]
-    task_completed = [todo for todo in todos if todo['completed']
-                      and todo['userId'] == id]
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = int(argv[1])
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    params = {'userId': user_id}
+    todos = requests.get(url + "todos", params=params).json()
+    task_completed = [todo['title'] for todo in todos if todo['completed']]
     print("Employee {} is done with tasks({}/{}):".format(
-        name, len(task_completed), len(task_total)))
+        user['name'], len(task_completed), len(todos)))
     [print('\t', x['title']) for x in task_completed]
