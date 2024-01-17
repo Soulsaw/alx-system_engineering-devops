f55ffacce36278f503713e39abdb72f3f1e0f70e
Soulsaw <ssawadogo292@gmail.com>
2024-01-17 06:33:59 +0000

    fix little

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,9 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the numbers of the giving subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    try:
-        results = requests.get(url=url)
-        if results.status_code == 200:
-            data = results.json()
-            return (data['data']['subscribers'])
-        else:
-            return (0)
-    except Exception as e:
-        pass
-    return (0)
+    results = requests.get(url=url, allow_redirects=False)
+    if results.status_code == 200:
+        data = results.json()
+        return (data['data']['subscribers'])
+    else:
+        return (0)
