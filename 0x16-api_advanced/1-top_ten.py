6d2356ac011ae62d6c0106452af360143406ac46
Soulsaw <ssawadogo292@gmail.com>
2024-01-17 00:24:13 +0000

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,5 +3,16 @@
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """Doc of the module"""
+def top_ten(subreddit):
+    """Return the 10 hot comments"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    try:
+        results = requests.get(url=url)
+        if results.status_code == 200:
+            data = results.json()
+            for comment in data['data']['children'][:10]:
+                print(comment['data']['title'])
+            return
+    except Exception as e:
+        pass
+    print(None)
